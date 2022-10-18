d8da77a5ee2ee4fcceac9fdbf035546af620e054
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-18 12:29:38 +0600

    added Button and Social media icon list group on Right Side Nav

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { FaGoogle, FaGithub, FaFacebook, FaYoutube, FaTwitter, FaWhatsapp, FaInstagram } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from 'react-router-dom';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h1>Right side nav</h1>
-        </div>
+            <Button variant="outline-primary mb-2 w-100">
+                <FaGoogle /> Login via Google</Button>
+            <Button variant="outline-dark mb-2 w-100">
+                <FaGithub /> Login via Github</Button>
+            <p className='h5 mt-3 mb-1'>Find us on</p>
+            <ListGroup>
+                <ListGroup.Item className='mb-3'><FaFacebook /> Facebook</ListGroup.Item>
+                <ListGroup.Item className='mb-3'><FaYoutube /> Youtube</ListGroup.Item>
+                <ListGroup.Item className='mb-3'><FaTwitter /> Twitter</ListGroup.Item>
+                <ListGroup.Item className='mb-3'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                <ListGroup.Item className='mb-3'><FaInstagram /> Instagram</ListGroup.Item>
+            </ListGroup>
+        </div >
     );
 };
 
