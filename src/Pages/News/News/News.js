689c9f3f409a7b5de4329ad098ace5e412a56622
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-20 10:30:47 +0600

    displayed news details on News component

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
+import { Link, useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const News = () => {
     const news = useLoaderData();
+    const { title, image_url, details, category_id } = news;
+    console.log(news)
     return (
         <div>
-            <h1>News</h1>
-            <NewsSummaryCard
-                key={news._id}
-                news={news}></NewsSummaryCard>
+            <Card style={{ width: 'auto' }}>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/category/${category_id}`}>
+                        <Button variant="primary">Show all news of this category</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
 
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg" className='mb-3'>
             <Container>
-                <Navbar.Brand href="#home">NEWS TODAY</Navbar.Brand>
+                <Link to='/' className='text-decoration-none'>
+                    <Navbar.Brand>NEWS TODAY</Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
